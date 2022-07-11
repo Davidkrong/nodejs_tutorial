2f2f748048ddfa8f5524b30071cf3bb93f63ecc3
David Krongauz <david@Davids-MacBook-Pro.local>
2022-07-11 11:15:39 +0300

    added rookout wrap and token

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 const server = require('./app')
 
-server.start()
+const rookout = require('rookout')
+rookout.start({ 
+    token: [f06399e9ac1da2c14d3fb172dbe053601acc64bb43e36ae468e53d6e0b6bd50c],
+    labels: {
+        env: dev
+    }
+}).then(server.start())
+
